22ef48d797faf01a9b7ef497cfdc78b5678a4639
as2120 <126.org@gmail.com>
2016-09-02 09:45:16 +0800

    d.2016-09-02.09_45_15.97.K.MzM.T@WS

--- code/obs/lame/mac/MacDLLMain.c
+++
@@ -1,1 +0,0 @@
-﻿/*	MacDLLMain.c	*/intmain(){	return	0;}
--- code/obs/minhook/dll_resources/dllmain.cpp
+++
@@ -1,7 +0,0 @@
-﻿#include <Windows.h>
-
-BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserved)
-{
-	return TRUE;
-}
-
