d4d686fe8671eef38eacdacda6cee09a18fee686
as2120 <126.org@gmail.com>
2017-04-07 11:24:23 +0800

    d.2017-04-07.11_24_18.82.K.MzM.T@WS.BG57IV3

--- code/leixiaohua1020/ffplayformfc/ffplaymfc/include/libavutil/audioconvert.h
+++
@@ -1,6 +0,0 @@
-﻿
-#include "version.h"
-
-#if FF_API_AUDIOCONVERT
-#include "channel_layout.h"
-#endif
--- code/leixiaohua1020/ffplayformfc/ffplaymfc/include/libavutil/ffversion.h
+++
@@ -1,4 +0,0 @@
-﻿#ifndef AVUTIL_FFVERSION_H
-#define AVUTIL_FFVERSION_H
-#define FFMPEG_VERSION "N-62994-g2baf1c8"
-#endif /* AVUTIL_FFVERSION_H */
